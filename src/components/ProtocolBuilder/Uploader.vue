71aa3a6f4f02b91a87e1e2760289284726f90e9b
devbtech <devtech728@gmail.com>
2021-01-27 15:54:56 +0200

    Created global uploader component

---
+++ src/components/ProtocolBuilder/Uploader.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    
+    <v-alert v-if="!structureTypes.includes(initialType)" dense type="info">
+      <p>Please choose what type of uploader you need.</p>
+      <h5>Uploader types:</h5>
+      <ul>
+        <li v-for="type in structureTypes" :key="type">{{type}}</li>
+      </ul>
+      <p class="mt-3">Use :initialType=" 'type of uploader' " property to spicify what uploader to show</p>
+    </v-alert>
+    <!-- Showing when user did not specify what type of uploader to show -->
+
+    <div v-if="initialType === 'image'">
+      Img Uploader
+    </div>
+    <!-- Images Uploader Structure -->
+
+    <div v-if="initialType === 'audio'">
+      Audio Uploader
+    </div>
+    <!-- Audio Uploader Structure -->
+
+    <v-dialog v-model="removeConfirm" persistent width="400">
+      <v-alert
+        border="left"
+        colored-border
+        type="error"
+        elevation="2"
+      >
+        <v-row class="flex-column">
+          <v-col class="grow">
+            <h3>Are you sure you want to remove "{{this.initialType}}" from Item?</h3>
+          </v-col>
+          <v-col class="shrink d-flex justify-end">
+            <v-btn
+              @click="removeConfirm = false"
+            >
+              Cancel
+            </v-btn>
+            <v-btn 
+              class="ml-4" 
+              color="error"
+              @click="onClickRemove"
+            >
+              Remove
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-alert>
+    </v-dialog>
+    <!-- Delete Confirmation Popup -->
+
+  </div>
+</template>
+
+<script>
+import Uploader from '../../models/Uploader';
+
+export default {
+  props: {
+    initialType: {
+      type: String,
+      default: '',
+    },
+    initialAdditionalType: {
+      type: String,
+      default: '',
+    },
+    initialData: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    const structureTypes = ['image', 'audio'];
+    const uploader = new Uploader(this.initialType);
+
+    return {
+      structureTypes,
+      uploader,
+      data: this.initialData,
+      removeConfirm: false,
+    };
+  },
+  methods: {
+
+    async uploadFile() {
+      try {
+        const response = await this.uploader.upload(this.data);
+        this.$emit('onUploaded', response.location);
+      } catch(err) {
+        this.data = this.initialData;
+        setTimeout(() => {
+          this.$emit('onError', { 
+            type: 'error', 
+            message: `Something went wrong with uploading "${this.initialType}${this.initialAdditionalType ? ': ' + this.initialAdditionalType : ''}". Please try to upload again or just save Item without adding ${this.initialType}.`,
+          });
+        }, 1000);
+      }
+    },
+
+    onClickRemove() {
+      const inputRef = this.$refs['fileInput'];
+      if(inputRef) inputRef.value = '';
+      this.$emit('onRemove');
+      this.data = '';
+      this.removeConfirm = false;
+    },
+
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.v-expansion-panel-content .v-btn { width: 100%; }
+
+.input-file-container {
+  position: relative;
+
+  .file-input, .file-input:after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+    border-radius: 4px;
+    z-index: 1;
+    opacity: 0;
+  }
+
+  .file-input {
+    &:after {
+      content: '';
+      cursor: pointer;
+    }
+
+    &:hover + .v-btn:before {
+      opacity: 0.08;
+    }
+  }
+
+}
+
+.v-dialog .v-alert {
+  margin-bottom: 0px
+}
+
+</style>

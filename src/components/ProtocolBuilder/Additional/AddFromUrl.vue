a0de4ddf096b959e505bfd17b1df709e86922a78
devbtech <devtech728@gmail.com>
2021-02-24 16:07:10 +0200

    Added add-from-url popup

---
+++ src/components/ProtocolBuilder/Additional/AddFromUrl.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-dialog v-model="show" persistent width="800">
+    <v-card>
+      <v-card-title class="headline grey lighten-2" primary-title>
+        <v-icon left large>mdi-link-variant-plus</v-icon>
+        Add from URL
+      </v-card-title>
+      <v-card-text>
+        <v-text-field label="URL" v-model="url" />
+      </v-card-text>
+      <v-divider />
+      <v-card-actions>
+        <v-btn 
+          outlined
+          color="primary"
+          @click="$emit('cancel')"
+        >
+          Cancel
+        </v-btn>
+        <v-spacer />
+        <v-btn 
+          color="primary"
+          :disabled="!url"
+          @click="$emit('add', url)"
+        >
+          Add
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    }
+  },
+  data: () => {
+    return {
+      url: '',
+    }
+  }
+}
+</script>

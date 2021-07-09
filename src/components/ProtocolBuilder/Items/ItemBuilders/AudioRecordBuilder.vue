c30ad9f76923ef4a0b5857446183f412fdbbaf10
barry <barrysunderland@outlook.com>
2021-07-09 23:38:01 +0200

    bugfix/AudioRecordBuilder disable skippable checkbox if option text required

--- src/components/ProtocolBuilder/Items/ItemBuilders/AudioRecordBuilder.vue
+++ src/components/ProtocolBuilder/Items/ItemBuilders/AudioRecordBuilder.vue
@@ -32,7 +32,7 @@
       :text="isOptionalText"
       :required="responseOptions.isOptionalTextRequired"
       @text="isOptionalText = $event; $emit('updateOptionalText', isOptionalText)"
-      @required="responseOptions.isOptionalTextRequired = $event; onUpdateResponseOptions();"
+      @required="updateRequired"
     />
   </v-form>
 </template>
@@ -88,6 +88,18 @@ export default {
     }
   },
   methods: {
+    updateRequired(event) {
+      // disable the skippable button if item is required 
+      if (event) {
+        this.isSkippable = false
+        this.isSkippableItem=2
+      } else {
+        this.isSkippableItem=0
+      }
+
+      this.responseOptions.isOptionalTextRequired = event;
+      this.onUpdateResponseOptions();
+    },
     update () {
       const responseOptions = {
         'schema:minValue': this.minValue,

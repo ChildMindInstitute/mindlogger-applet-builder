42b55b8adbee7151ec314657fcd3eebb4e56fbca
mtg137 <jony374@outlook.com>
2021-11-23 03:54:22 -0500

    update image resolution

--- src/components/ProtocolBuilder/Items/ItemBuilders/tokenSummary.js
+++ src/components/ProtocolBuilder/Items/ItemBuilders/tokenSummary.js
@@ -1,6 +1,6 @@
 export const tokenSummary = {
 	"name": "token_summary",
-	"question": "\r\n\r\n![token_summary.png](https://raw.githubusercontent.com/ChildMindInstitute/mindlogger-applet-builder/master/src/assets/icons/item-types/token-summary.png =150x150)\r\n\r\nGreat job! Here's summary of the activity you just completed!",
+	"question": "\r\n\r\n![token_summary.png](https://raw.githubusercontent.com/ChildMindInstitute/mindlogger-applet-builder/master/src/assets/icons/item-types/token-summary.png =100x100)\r\n\r\nGreat job! Here's summary of the activity you just completed!",
 	"description": "",
 	"ui": {
 		"allow": [
